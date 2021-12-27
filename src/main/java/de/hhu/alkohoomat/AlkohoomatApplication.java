e6bbe526217ad566e19cc6ef1966a2506eca3ca9
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-27 23:53:03 +0100

    initial commit

---
+++ src/main/java/de/hhu/alkohoomat/AlkohoomatApplication.java
@@ -0,0 +1,13 @@
+package de.hhu.alkohoomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkohoomatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlkohoomatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/hhu/alkohoomat/AlkohoomatApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.alkohoomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkohoomatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
