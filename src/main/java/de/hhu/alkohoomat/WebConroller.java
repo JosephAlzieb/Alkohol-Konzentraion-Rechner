5aac80af9415df6de366961dda5f0987cc464d7c
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-28 00:55:58 +0100

    post methode hinzugefügt, die die AlkoholKonzentration berechnet und zurückgibt.

--- src/main/java/de/hhu/alkohoomat/WebConroller.java
+++ src/main/java/de/hhu/alkohoomat/WebConroller.java
@@ -1,7 +1,9 @@
 package de.hhu.alkohoomat;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebConroller {
@@ -9,5 +11,12 @@ public class WebConroller {
     @GetMapping("/alko")
     String getStartSeite(){
         return "index";
+    }
+
+    @PostMapping("/alko")
+    String berechnekoholkonz (Model model, Form form){
+        String reult = form.getBlutalkohol();
+        model.addAttribute("result", reult);
+        return"index";
     }
 }
