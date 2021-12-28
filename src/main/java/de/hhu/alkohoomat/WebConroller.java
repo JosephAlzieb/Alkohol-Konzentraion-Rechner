5ecdf890d9226cd2096496c5cc280d24ee878631
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2021-12-28 01:18:05 +0100

    ein Formular mit dfault-werte in die html-seite injected, um es später auszufüllen.

--- src/main/java/de/hhu/alkohoomat/WebConroller.java
+++ src/main/java/de/hhu/alkohoomat/WebConroller.java
@@ -3,18 +3,21 @@ package de.hhu.alkohoomat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebConroller {
 
     @GetMapping("/alko")
-    String getStartSeite(){
+    String getStartSeite(Model model){
+        Form form = new Form(65,0,0,0,0);
+        model.addAttribute("form", form);
         return "index";
     }
 
     @PostMapping("/alko")
-    String berechnekoholkonz (Model model, Form form){
+    String berechnekoholkonz (Model model, @ModelAttribute("form") Form form){
         String reult = form.getBlutalkohol();
         model.addAttribute("result", reult);
         return"index";
